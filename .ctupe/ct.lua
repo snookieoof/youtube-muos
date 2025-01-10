2a85df61fa35df7a71737cbf92f43081b0083a91
cuongnv <cnv>
2025-01-10 11:29:34 +0900

    update

--- .ctupe/ct.lua
+++ .ctupe/ct.lua
@@ -9,29 +9,10 @@ end
 
 function CT.Play(url)
     -- local command = string.format(". /opt/muos/script/launch/ext-mpv-ctupe.sh %s %s", "CTupe" , Config.MEDIA_PATH)
-
-    local command = string.format(". /opt/muos/script/launch/ext-ffplay-ctupe.sh %s", url)
-
+    -- local command = string.format(". /opt/muos/script/launch/ext-ffplay-ctupe.sh %s", url)
     -- local command = "youtube-dl \"".. url .."\" -o - | ffplay - -autoexit -loglevel quiet"
 
-    -- local command =
-    -- [[
-    --     . /opt/muos/script/var/func.sh
-    --     HOME="$(GET_VAR "device" "board/home")"
-    --     export HOME
-
-    --     SDL_HQ_SCALER="$(GET_VAR "device" "sdl/scaler")"
-    --     SDL_ROTATION="$(GET_VAR "device" "sdl/rotation")"
-    --     SDL_BLITTER_DISABLED="$(GET_VAR "device" "sdl/blitter_disabled")"
-    --     export SDL_HQ_SCALER SDL_ROTATION SDL_BLITTER_DISABLED
-
-    --     SET_VAR "system" "foreground_process" "ffplay"
-
-    --     youtube-dl "]] .. url .. [[" -o - | ffplay - -autoexit -loglevel quiet > output.txt 2>&1
-
-    --     unset SDL_HQ_SCALER SDL_ROTATION SDL_BLITTER_DISABLED
-    -- ]]
-
+    local command = string.format(Config.PLAY_CMD, url)
     os.execute(command)
 end
 
--- .ctupe/data/ext-ffplay-ctupe.sh
+++ .ctupe/data/ext-ffplay-ctupe.sh
@@ -2,7 +2,10 @@
 
 . /opt/muos/script/var/func.sh
 
-URL=$1
+URL="%s"
+
+GPTOKEYB="$(GET_VAR "device" "storage/rom/mount")/MUOS/emulator/gptokeyb/gptokeyb2"
+APP_DIR="/mnt/mmc/MUOS/application/.ctupe/data"
 
 HOME="$(GET_VAR "device" "board/home")"
 export HOME
@@ -12,8 +15,13 @@ SDL_ROTATION="$(GET_VAR "device" "sdl/rotation")"
 SDL_BLITTER_DISABLED="$(GET_VAR "device" "sdl/blitter_disabled")"
 export SDL_HQ_SCALER SDL_ROTATION SDL_BLITTER_DISABLED
 
+export SDL_GAMECONTROLLERCONFIG_FILE="/usr/lib/gamecontrollerdb.txt"
+
 SET_VAR "system" "foreground_process" "ffplay"
 
-youtube-dl "$URL" -o - | /usr/bin/ffplay - -autoexit -loglevel quiet
+$GPTOKEYB "ffplay" -c "$APP_DIR/general_ffplay.gptk" &
+ youtube-dl "$URL" -o - | /usr/bin/ffplay - -autoexit -loglevel quiet
+
+killall -q gptokeyb2
 
 unset SDL_HQ_SCALER SDL_ROTATION SDL_BLITTER_DISABLED
